6fb23db5f56225092d7122ad05371ccf5b77b23c
Bullish Design <BullishDesignLLC@gmail.com>
2024-10-12 18:37:35 -0500

    Starting on Typed LLM Call functionality.

---
+++ aider/llm_wrapper.py
@@ -0,0 +1,6 @@
+# An attempt to wrap the existing call to litellm in order to use mirascope for structured calls.
+# Should default back to a plain call if a typed call is not found.
+#      - Or default to passing back the plain response?
+#      - Or default to restructuring the plain response and passig that?
+
+
